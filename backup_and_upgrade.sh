ebac72c3f46c2d8b121c5a02a5caaac51688188f
jan <jan@kwoh.de>
2019-02-17 11:19:31 +0100

    update only if new image

--- backup_and_upgrade.sh
+++ backup_and_upgrade.sh
@@ -5,33 +5,57 @@ BACKUP_FILE="db_`date +%Y-%m-%d`.dump"
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 
-## stop miniflux
-echo "stopping..."
-docker-compose stop miniflux
+stop_and_flush() {
+    docker-compose stop miniflux
 
-## flush user sessions
-echo "flushing sessions..."
-docker-compose run --rm miniflux miniflux -flush-sessions
+    ## flush user sessions
+    docker-compose run --rm miniflux miniflux -flush-sessions
+}
 
-## make backup
-# docker-compose exec -u <your_postgres_user> <postgres_service_name> pg_dump -Fc <database_name_here> > db.dump
-echo "backing up..."
-docker-compose exec db pg_dump -U miniflux miniflux > ${BACKUP_FILE}
+backup() {
+    docker-compose exec db pg_dump -U miniflux miniflux > ${BACKUP_FILE}
 
-source .restic.env
-restic backup ${BACKUP_FILE}
-echo "removing local backup..."
-rm ${BACKUP_FILE}
+    source .restic.env
+    restic backup ${BACKUP_FILE}
+    echo "removing local backup..."
+    rm ${BACKUP_FILE}
+}
 
-## delete container
-echo "deleting container..."
-docker-compose rm --force miniflux 
+migrate() {
+    docker-compose rm --force miniflux
 
-## migrate db
-echo "migrating..."
-docker-compose run --rm miniflux miniflux -migrate
+    ## migrate db
+    echo "migrating..."
+    docker-compose run --rm miniflux miniflux -migrate
+}
 
-## start!
-echo "done, starting!"
-docker-compose up -d
 
+IMAGE_BEFORE_PULL=`docker image inspect miniflux/miniflux --format="{{.Config.Image}}"`
+docker pull miniflux/miniflux
+IMAGE_AFTER_PULL=`docker image inspect miniflux/miniflux --format="{{.Config.Image}}"`
+
+if [ "${IMAGE_BEFORE_PULL}" != "${IMAGE_AFTER_PULL}" ]; then
+    # we need to update
+
+    ## stop miniflux
+    echo "stopping..."
+    stop_and_flush
+
+    ## make backup
+    # docker-compose exec -u <your_postgres_user> <postgres_service_name> pg_dump -Fc <database_name_here> > db.dump
+    echo "backing up..."
+    backup
+
+    ## delete container
+    echo "deleting container..."
+    migrate
+
+    ## start!
+    echo "done, starting!"
+    docker-compose up -d
+
+else
+    # nothing changed, just backup
+    echo "backing up..."
+    backup
+fi
